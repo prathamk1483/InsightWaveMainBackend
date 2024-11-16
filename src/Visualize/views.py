f60631c12c6fe6ffa802e307c4ed80eb62327f48
Pratham Kubetkar <kubetkarpratham14@gmail.com>
2024-11-16 12:53:12 +0530

    Recovering 2

--- src/Visualize/views.py
+++ src/Visualize/views.py
@@ -13,53 +13,53 @@ from PIL import Image
 import seaborn as sns
 import google.generativeai as genai
 
-# matplotlib.use('Agg')
-# my_api_key= "AIzaSyDOs2aSdgZkypnyggSwVwXTaMfc7Vc2ABg"
-# genai.configure(api_key=my_api_key)
+matplotlib.use('Agg')
+my_api_key= "AIzaSyDOs2aSdgZkypnyggSwVwXTaMfc7Vc2ABg"
+genai.configure(api_key=my_api_key)
 
-# @api_view(["POST"])
-# def index(request):
-#     if request.method == "POST":
-#         data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
-#         print(data.head())
-#         return Response("On the Visualize.view.index file , the above is the data received")
-#     return HttpResponse("Ran the index file")
+@api_view(["POST"])
+def index(request):
+    if request.method == "POST":
+        data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
+        print(data.head())
+        return Response("On the Visualize.view.index file , the above is the data received")
+    return HttpResponse("Ran the index file")
 
 
-# @api_view(["POST"])
-# def histogram(request):
-#     if request.method == "POST":
-#         body = request.body.decode('utf-8')
-#         data = json.loads(body)
-#         cols = data.get('cols')
-#         data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
+@api_view(["POST"])
+def histogram(request):
+    if request.method == "POST":
+        body = request.body.decode('utf-8')
+        data = json.loads(body)
+        cols = data.get('cols')
+        data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
         
-#         plt.hist(data[f"{cols}"], bins=30, color='skyblue', edgecolor='black')
-#         plt.xlabel(f'{cols}')
-#         plt.ylabel('Frequency')
-#         plt.title('Basic Histogram')
+        plt.hist(data[f"{cols}"], bins=30, color='skyblue', edgecolor='black')
+        plt.xlabel(f'{cols}')
+        plt.ylabel('Frequency')
+        plt.title('Basic Histogram')
 
-#         buffer = BytesIO()
-#         plt.savefig(buffer, format='png')
-#         plt.close()
-#         buffer.seek(0)
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        buffer.seek(0)
 
-#         image_url = cloudinary.uploader.upload(buffer, folder="histograms/")
+        image_url = cloudinary.uploader.upload(buffer, folder="histograms/")
 
-#         url = image_url.get('secure_url')
+        url = image_url.get('secure_url')
 
-#         description = GenerateVisualizationDescription(url)
+        description = GenerateVisualizationDescription(url)
 
-#         final_response = {
-#             "url": url,
-#             "cols": cols,
-#             "generated_description": description
-#         }
+        final_response = {
+            "url": url,
+            "cols": cols,
+            "generated_description": description
+        }
 
-#         return Response(final_response)
+        return Response(final_response)
 
 
-#     return Response({"message":"Getting the histogram of the image"})
+    return Response({"message":"Getting the histogram of the image"})
 
 
 # @api_view(["POST"])
@@ -178,22 +178,22 @@ import google.generativeai as genai
 #     return Response("Failed to generate scatterplot")
 
 
-# def GenerateVisualizationDescription(image_url):
-#     response = requests.get(image_url)
-#     img = Image.open(BytesIO(response.content))
+def GenerateVisualizationDescription(image_url):
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
 
-#     buffered = BytesIO()
-#     img.save(buffered, format="PNG")
-#     img_bytes = buffered.getvalue()
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    img_bytes = buffered.getvalue()
 
-#     model = genai.GenerativeModel("gemini-1.5-flash")
-#     contents = [
-#         {"text": "Draw important insights from this visualization of a dataset ,act like you know about this dataset"},
-#         {
-#             "data": img_bytes, 
-#             "mime_type": "image/png"
-#         }
-#     ]
-#     response = model.generate_content(contents)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    contents = [
+        {"text": "Draw important insights from this visualization of a dataset ,act like you know about this dataset"},
+        {
+            "data": img_bytes, 
+            "mime_type": "image/png"
+        }
+    ]
+    response = model.generate_content(contents)
 
-#     return response.text
+    return response.text
