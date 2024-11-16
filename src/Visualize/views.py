8b6c719cd484f62b4c1ba22d45166efa5651a93b
Pratham Kubetkar <kubetkarpratham14@gmail.com>
2024-11-16 12:58:43 +0530

    Recovering 4

--- src/Visualize/views.py
+++ src/Visualize/views.py
@@ -62,120 +62,120 @@ def histogram(request):
     return Response({"message":"Getting the histogram of the image"})
 
 
-# @api_view(["POST"])
-# def correlation_heatmap(request):
-#     if "csv_data" in request.session:
-#         data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
-#         plt.figure(figsize=(10, 8))
-#         sns.heatmap(data.corr(), annot=True, cmap='coolwarm')
-#         plt.title('Correlation Heatmap')
+@api_view(["POST"])
+def correlation_heatmap(request):
+    if "csv_data" in request.session:
+        data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(data.corr(), annot=True, cmap='coolwarm')
+        plt.title('Correlation Heatmap')
 
-#         buffer = BytesIO()
-#         plt.savefig(buffer, format='png')
-#         plt.close()
-#         buffer.seek(0)
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        buffer.seek(0)
 
-#         response = cloudinary.uploader.upload(buffer, folder="heatmaps/")
-#         url = response['secure_url']
+        response = cloudinary.uploader.upload(buffer, folder="heatmaps/")
+        url = response['secure_url']
 
-#         description = GenerateVisualizationDescription(url)
+        description = GenerateVisualizationDescription(url)
 
-#         return Response({
-#             "url": url,
-#             "generated_description": description
-#         })
+        return Response({
+            "url": url,
+            "generated_description": description
+        })
 
-#     return Response("Failed to generate heatmap")
+    return Response("Failed to generate heatmap")
 
 
-# @api_view(["POST"])
-# def pairplot(request):
-#     if "csv_data" in request.session:
-#         data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
-#         sns.pairplot(data)
-#         plt.title('Pairplot')
+@api_view(["POST"])
+def pairplot(request):
+    if "csv_data" in request.session:
+        data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
+        sns.pairplot(data)
+        plt.title('Pairplot')
 
-#         # Save the plot to a file
-#         buffer = BytesIO()
-#         plt.savefig(buffer, format='png')
-#         plt.close()
-#         buffer.seek(0)
+        # Save the plot to a file
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        buffer.seek(0)
 
-#         response = cloudinary.uploader.upload(buffer, folder="Pairplots/")
-#         url = response['secure_url']
+        response = cloudinary.uploader.upload(buffer, folder="Pairplots/")
+        url = response['secure_url']
 
-#         description = GenerateVisualizationDescription(url)
+        description = GenerateVisualizationDescription(url)
         
-#         return Response({
-#             "url": url,
-#             "generated_description": description
-#         })
+        return Response({
+            "url": url,
+            "generated_description": description
+        })
 
-#     return Response("Failed to generate pairplot")
+    return Response("Failed to generate pairplot")
 
 
 
-# @api_view(["POST"])
-# def boxplot(request):
-#     if "csv_data" in request.session:
-#         body = request.body.decode('utf-8')
-#         data = json.loads(body)
-#         column = data.get('cols')
+@api_view(["POST"])
+def boxplot(request):
+    if "csv_data" in request.session:
+        body = request.body.decode('utf-8')
+        data = json.loads(body)
+        column = data.get('cols')
 
-#         data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
-#         sns.boxplot(data[column])
-#         plt.title(f'Boxplot of {column}')
+        data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
+        sns.boxplot(data[column])
+        plt.title(f'Boxplot of {column}')
 
-#         buffer = BytesIO()
-#         plt.savefig(buffer, format='png')
-#         plt.close()
-#         buffer.seek(0)
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        buffer.seek(0)
 
-#         response = cloudinary.uploader.upload(buffer, folder="BoxPlots/")
-#         url = response['secure_url']
+        response = cloudinary.uploader.upload(buffer, folder="BoxPlots/")
+        url = response['secure_url']
 
-#         description = GenerateVisualizationDescription(url)
+        description = GenerateVisualizationDescription(url)
         
-#         return Response({
-#             "url": url,
-#             "generated_description": description
-#         })
+        return Response({
+            "url": url,
+            "generated_description": description
+        })
 
-#     return Response("Failed to generate boxplot")
-
-
+    return Response("Failed to generate boxplot")
 
 
-# @api_view(["POST"])
-# def scatterplot(request):
-#     if "csv_data" in request.session:
-#         body = request.body.decode('utf-8')
-#         data = json.loads(body)
-#         x_col = data.get('x_col')
-#         y_col = data.get('y_col')
 
-#         data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
-#         sns.scatterplot(x=data[x_col], y=data[y_col])
-#         plt.xlabel(x_col)
-#         plt.ylabel(y_col)
-#         plt.title(f'Scatter Plot of {x_col} vs {y_col}')
 
-#         buffer = BytesIO()
-#         plt.savefig(buffer, format='png')
-#         plt.close()
-#         buffer.seek(0)
+@api_view(["POST"])
+def scatterplot(request):
+    if "csv_data" in request.session:
+        body = request.body.decode('utf-8')
+        data = json.loads(body)
+        x_col = data.get('x_col')
+        y_col = data.get('y_col')
 
-#         response = cloudinary.uploader.upload(buffer, folder="ScatterPlot/")
-#         url = response['secure_url']
+        data = pd.DataFrame(pd.read_json(StringIO(request.session["csv_data"]), orient='split'))
+        sns.scatterplot(x=data[x_col], y=data[y_col])
+        plt.xlabel(x_col)
+        plt.ylabel(y_col)
+        plt.title(f'Scatter Plot of {x_col} vs {y_col}')
 
-#         description = GenerateVisualizationDescription(url)
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        buffer.seek(0)
+
+        response = cloudinary.uploader.upload(buffer, folder="ScatterPlot/")
+        url = response['secure_url']
+
+        description = GenerateVisualizationDescription(url)
         
-#         return Response({
-#             "url": url,
-#             "generated_description": description
-#         })
+        return Response({
+            "url": url,
+            "generated_description": description
+        })
 
-#     return Response("Failed to generate scatterplot")
+    return Response("Failed to generate scatterplot")
 
 
 def GenerateVisualizationDescription(image_url):
